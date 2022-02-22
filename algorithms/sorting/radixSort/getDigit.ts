20b9925faa6d9288ff097b30f0d9ab072fd74ec0
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-02-22 08:23:59 +0200

    algorithms\sorting\radixSort\getDigit.ts

---
+++ algorithms/sorting/radixSort/getDigit.ts
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/a/41712226/7982963
+export default function getDigit(number: number, index: number): number {
+  return 0;
+}
+
+console.log(getDigit(1234, 0)); // 4
+console.log(getDigit(1234, 1)); // 3
---
+++ algorithms/sorting/radixSort/radixSort.ts
@@ -0,0 +1,1 @@
+// Just watch: https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/8344146#overview
