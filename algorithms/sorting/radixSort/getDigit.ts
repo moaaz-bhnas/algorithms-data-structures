be8f6bd6438e25572e7dabee55a4306ed7eae671
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-02-23 07:05:23 +0200

    algorithms\sorting\radixSort\getDigit.ts

--- algorithms/sorting/radixSort/getDigit.ts
+++ algorithms/sorting/radixSort/getDigit.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/41712226/7982963
 export default function getDigit(number: number, index: number): number {
-  return 0;
+  return Math.floor((number / Math.pow(10, index)) % 10);
 }
 
 console.log(getDigit(1234, 0)); // 4
